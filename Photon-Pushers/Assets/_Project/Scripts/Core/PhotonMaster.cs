da2a1e020c0d78e07f2a5bda99cca20a8ed89214
GishaTheDeveloper <spaceshhz@gmail.com>
2021-04-18 13:27:35 +0300

    fix bug with error on player spawn

--- Photon-Pushers/Assets/_Project/Scripts/Core/PhotonMaster.cs
+++ Photon-Pushers/Assets/_Project/Scripts/Core/PhotonMaster.cs
@@ -33,7 +33,7 @@ namespace Gisha.Pushers.Core
         public override void OnJoinedRoom()
         {
             Debug.Log("Successfully connected to room.");
-            Vector3 position = GameManager.Spawnpoints[PhotonNetwork.PlayerList.Length - 1].position;
+            Vector3 position = GameManager.Spawnpoints[Mathf.Max(PhotonNetwork.PlayerList.Length - 1, 0)].position;
             PhotonNetwork.Instantiate("Player", position, Quaternion.identity);
         }
     }
