ea5903e0db1411e8c3fbaff2ec553c51650ddf1c
GishaTheDeveloper <spaceshhz@gmail.com>
2021-04-18 15:01:34 +0300

    add push syncing using raycast

--- Photon-Pushers/Assets/_Project/Scripts/Core/PlayerMovement.cs
+++ Photon-Pushers/Assets/_Project/Scripts/Core/PlayerMovement.cs
@@ -5,7 +5,15 @@ namespace Gisha.Pushers.Core
 {
     public class PlayerMovement : MonoBehaviour
     {
+        [Header("Movement")]
         [SerializeField] private float movementSpeed;
+
+        [Header("Push")]
+        [SerializeField] private float pushMagnitude;
+        [SerializeField] private float raycastDst;
+
+        Vector3 _angularInput;
+        Vector3 _pushInput;
 
         Rigidbody _rb;
         PhotonView _pv;
@@ -21,8 +29,46 @@ namespace Gisha.Pushers.Core
             if (!_pv.IsMine)
                 return;
 
-            Vector3 angularInput = new Vector3(-Input.GetAxis("Vertical"), 0f, Input.GetAxis("Horizontal"));
-            _rb.angularVelocity = angularInput * movementSpeed;
+            Move();
+            CheckForPush();
+        }
+
+        private void Move()
+        {
+            _angularInput = new Vector3(-Input.GetAxis("Vertical"), 0f, Input.GetAxis("Horizontal"));
+            _rb.angularVelocity = _angularInput * movementSpeed;
+        }
+
+        private void CheckForPush()
+        {
+            _pushInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")).normalized;
+            _pushInput *= -1f;
+
+            if (Physics.Raycast(transform.position, _pushInput, out RaycastHit hitInfo, raycastDst))
+            {
+                if (hitInfo.collider.CompareTag("Player"))
+                {
+                    var pv = hitInfo.collider.GetComponent<PhotonView>();
+
+                    if (!pv.IsMine)
+                    {
+                        var pushForce = _pushInput * pushMagnitude;
+                        pv.RPC("PushMe", RpcTarget.All, pushForce);
+                    }
+                }
+            }
+        }
+
+        [PunRPC]
+        private void PushMe(Vector3 pushForce)
+        {
+            _rb.AddForce(pushForce);
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(transform.position, _pushInput * raycastDst);
         }
     }
 }
