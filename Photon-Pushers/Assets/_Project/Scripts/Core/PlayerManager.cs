26ea876110ded0bf2eba12021f4a1d9258c0e6de
GishaTheDeveloper <spaceshhz@gmail.com>
2021-04-19 23:06:30 +0300

    add player highlight

--- Photon-Pushers/Assets/_Project/Scripts/Core/PlayerManager.cs
+++ Photon-Pushers/Assets/_Project/Scripts/Core/PlayerManager.cs
@@ -5,9 +5,12 @@ namespace Gisha.Pushers.Core
 {
     public class PlayerManager : MonoBehaviour
     {
+        [SerializeField] private GameObject playerHighlightPrefab;
+
         public Transform Spawnpoint { get; private set; }
         public GameObject Player { get; private set; }
 
+        Transform _playerHighlight;
         PhotonView _pv;
 
         private void Awake()
@@ -22,6 +25,7 @@ namespace Gisha.Pushers.Core
 
             Spawnpoint = GameManager.Spawnpoints[Mathf.Max(PhotonNetwork.PlayerList.Length - 1, 0)];
             Player = PhotonNetwork.Instantiate("Player", Spawnpoint.position, Quaternion.identity);
+            _playerHighlight = Instantiate(playerHighlightPrefab, Player.transform.position, playerHighlightPrefab.transform.rotation).transform;
         }
 
         private void LateUpdate()
@@ -31,11 +35,18 @@ namespace Gisha.Pushers.Core
 
             if (Player.transform.position.y < -25f)
                 PlayerRespawn();
+
+            UpdateHighlightPosition();
         }
 
         private void PlayerRespawn()
         {
             Player.transform.position = Spawnpoint.position;
+        }
+
+        private void UpdateHighlightPosition()
+        {
+            _playerHighlight.position = Player.transform.position - Vector3.up * 0.85f;
         }
     }
 }
