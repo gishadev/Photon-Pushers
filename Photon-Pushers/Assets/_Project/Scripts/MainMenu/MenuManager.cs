c022bafef53cf31d86c4039735438432e3602dc1
GishaTheDeveloper <spaceshhz@gmail.com>
2021-04-24 21:22:01 +0300

    update menu UI

---
+++ Photon-Pushers/Assets/_Project/Scripts/MainMenu/MenuManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Gisha.Pushers.MainMenu
+{
+    public class MenuManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] menus;
+
+        public void OnClick_ChangeMenu(int menuToChange)
+        {
+            for (int i = 0; i < menus.Length; i++)
+                menus[i].SetActive(false);
+            menus[menuToChange].SetActive(true);
+        }
+
+    }
+}
