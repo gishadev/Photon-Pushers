6688a79a001a8b2c2d434746edc1e3971950f6c5
GishaTheDeveloper <spaceshhz@gmail.com>
2021-04-30 00:17:46 +0300

    add connecting to a server message

--- Photon-Pushers/Assets/_Project/Scripts/MainMenu/MenuManager.cs
+++ Photon-Pushers/Assets/_Project/Scripts/MainMenu/MenuManager.cs
@@ -1,18 +1,34 @@
+using Photon.Pun;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Gisha.Pushers.MainMenu
 {
-    public class MenuManager : MonoBehaviour
+    public class MenuManager : MonoBehaviourPunCallbacks
     {
         #region Singleton
         public static MenuManager Instance { get; private set; }
         #endregion
 
         [SerializeField] private GameObject[] menus;
+        [SerializeField] private GameObject connectingText;
+        [SerializeField] private Button playBtn;
 
         private void Awake()
         {
             Instance = this;
+        }
+
+        public override void OnJoinedLobby()
+        {
+            connectingText.SetActive(false);
+            playBtn.interactable = true;
+        }
+
+        public override void OnLeftLobby()
+        {
+            connectingText.SetActive(true);
+            playBtn.interactable = false;
         }
 
         public void ChangeMenu(int menuToChange)
