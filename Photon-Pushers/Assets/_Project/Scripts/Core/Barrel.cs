2c6f5cb6b8aa188940a1add722f2a2f631de87c8
GishaTheDeveloper <spaceshhz@gmail.com>
2021-05-01 22:07:31 +0300

    add explosion barrel

---
+++ Photon-Pushers/Assets/_Project/Scripts/Core/Barrel.cs
@@ -0,0 +1,41 @@
+using System.Linq;
+using UnityEngine;
+
+namespace Gisha.Pushers.Core
+{
+    public class Barrel : MonoBehaviour
+    {
+        [SerializeField] private float explosionForce;
+        [SerializeField] private float explosionRadius;
+        [SerializeField] private GameObject explosionPrefab;
+
+        private void Explode()
+        {
+            var rigidbodies = FindObjectsOfType<PlayerMovement>()
+                .Select(x => x.GetComponent<Rigidbody>())
+                .ToArray();
+
+            foreach(var rb in rigidbodies)
+            {
+                var direction = (rb.transform.position - transform.position).normalized;
+                rb.AddForce(direction * explosionForce);
+            }
+
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+
+            Destroy(gameObject);
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.collider.CompareTag("Player"))
+                Explode();
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, explosionRadius);
+        }
+    }
+}
